CyanogenMod/android_packages_apps_Phone

Add compatibility code with mainline's configuration options for unlock
Change-Id: Ife2d6fad5cb1e7c846f5969401ee6dbc881aea3f

src/com/android/phone/PhoneGlobals.java
@@ -320,7 +320,8 @@ public class PhoneGlobals extends ContextWrapper
                 // TODO: This event should be handled by the lock screen, just
                 // like the "SIM missing" and "Sim locked" cases (bug 1804111).
                 case EVENT_PERSO_LOCKED:
-                    if (getResources().getBoolean(R.bool.ignore_sim_perso_locked_events)) {
+                    if (getResources().getBoolean(R.bool.ignore_sim_perso_locked_events) ||
+                        getResources().getBoolean(R.bool.ignore_sim_network_locked_events)) {
                         // Some products don't have the concept of a "SIM network lock"
                         Log.i(LOG_TAG, "Ignoring EVENT_PERSO_LOCKED event; "
                               + "not showing 'SIM network unlock' PIN entry screen");

Euphoria-OS-Legacy/android_packages_services_Telephony

src/com/android/phone/PhoneGlobals.java
@@ -228,7 +228,8 @@ public class PhoneGlobals extends ContextWrapper {
                 // TODO: This event should be handled by the lock screen, just
                 // like the "SIM missing" and "Sim locked" cases (bug 1804111).
                 case EVENT_PERSO_LOCKED:
-                    if (getResources().getBoolean(R.bool.ignore_perso_locked_events)) {
+                    if (getResources().getBoolean(R.bool.ignore_perso_locked_events) ||
+                        getResources().getBoolean(R.bool.ignore_sim_network_locked_events)) {
                         // Some products don't have the concept of a "SIM network lock"
                         Log.i(LOG_TAG, "Ignoring EVENT_PERSO_LOCKED event; "
                               + "not showing 'PERSO unlock' PIN entry screen");